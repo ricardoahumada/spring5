8a68ff6e94a100b7b073fed5a4a5c1e4616a5042
ricardonetmind <ricardoag@expert.netmind.es>
2022-06-03 10:07:22 +0200

    fix srp

--- FisrtSpring/src/main/java/com/netmind/App.java
+++ FisrtSpring/src/main/java/com/netmind/App.java
@@ -7,8 +7,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("file:src/main/resources/beans.xml");
-        Message obj = (Message) context.getBean("helloWorld");
-        obj.getMessage();
+
+        try {
+            Message obj = (Message) context.getBean("aMessage");
+            String mss= obj.getMessage();
+            System.out.println("Your Message : " + mess);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
--- FisrtSpring/src/main/java/com/netmind/Message.java
+++ FisrtSpring/src/main/java/com/netmind/Message.java
@@ -1,14 +1,14 @@
 package com.netmind;
 
 public class Message {
-	private String message;
+    private String message;
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public void getMessage() {
-		System.out.println("Your Message : " + message);
-	}
+    public String getMessage() {
+        return this.message;
+    }
 
 }
