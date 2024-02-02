e8f54fcc836b11172ddf43ce0a94124f8f23b7c5
ricardonetmind <ricardoag@expert.netmind.es>
2024-02-02 11:54:19 +0100

    update

--- BananaWhatsApp/src/test/java/com/banana/bananawhatsapp/persistencia/UsuarioRepositoryTest.java
+++ BananaWhatsApp/src/test/java/com/banana/bananawhatsapp/persistencia/UsuarioRepositoryTest.java
@@ -1,42 +1,16 @@
 package com.banana.bananawhatsapp.persistencia;
 
-import com.banana.bananawhatsapp.config.SpringConfig;
-import com.banana.bananawhatsapp.modelos.Usuario;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDate;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {SpringConfig.class})
-@ActiveProfiles("dev")
 class UsuarioRepositoryTest {
-    @Autowired
     IUsuarioRepository repo;
 
     @Test
-    void dadoUnUsuarioValido_cuandoCrear_entoncesUsuarioValido() throws Exception {
-        Usuario nuevo = new Usuario(null, "Ricardo", "r@r.com", LocalDate.now(), true);
-        repo.crear(nuevo);
-
-        assertThat(nuevo, notNullValue());
-        assertThat(nuevo.getId(), greaterThan(0));
+    void dadoUnUsuarioValido_cuandoCrear_entoncesUsuarioValido() {
     }
 
     @Test
     void dadoUnUsuarioNOValido_cuandoCrear_entoncesExcepcion() {
-        Usuario nuevo = new Usuario(null, "Ricardo", "r", LocalDate.now(), true);
-        assertThrows(Exception.class, () -> {
-            repo.crear(nuevo);
-        });
     }
 
     @Test
