b435ffe0740345e87683ba24bdf47896cd26cd96
ricardonetmind <ricardoag@expert.netmind.es>
2024-02-06 22:54:12 +0100

    update

---
+++ SchoolManagerHbnt/src/main/java/com/banana/config/ReposConfig.java
@@ -0,0 +1,39 @@
+package com.banana.config;
+
+import com.banana.persistence.SchoolsRepositoryInf;
+import com.banana.persistence.StudentsRepository;
+import com.banana.persistence.StudentsRepositoryInf;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class ReposConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${db.conn}")
+    String dbUrl;
+
+    @Value("${max.conn}")
+    Integer maxConn;
+
+
+    @Bean
+    public StudentsRepositoryInf getStudentsRepository() {
+        System.out.println("maxConn:" + maxConn);
+
+        String dbUrlEnv = env.getProperty("db.conn", String.class);
+        System.out.println("dbUrlEnv:" + dbUrlEnv);
+
+        StudentsRepository repo = new StudentsRepository();
+        repo.setUrlConn(dbUrl);
+        return repo;
+    }
+
+
+}
