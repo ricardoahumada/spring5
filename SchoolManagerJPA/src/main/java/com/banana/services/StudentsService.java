479293bb6c228abc5169760113d2b58073a78d55
Ricardo <ricardo@enmotionvalue.com>
2024-02-09 11:14:54 +0100

    update

--- SchoolManagerJPA/src/main/java/com/banana/services/StudentsService.java
+++ SchoolManagerJPA/src/main/java/com/banana/services/StudentsService.java
@@ -1,17 +1,24 @@
 package com.banana.services;
 
+import com.banana.models.School;
 import com.banana.models.Student;
+import com.banana.persistence.SchoolsRepositoryInf;
 import com.banana.persistence.StudentsRepository;
 import com.banana.persistence.StudentsRepositoryInf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 //@Component
 @Service
 public class StudentsService implements IStudentService {
     @Autowired
     private StudentsRepositoryInf repository;
+
+    @Autowired
+    private SchoolsRepositoryInf repoEscuela;
 
     public void setRepository(StudentsRepositoryInf repository) {
         this.repository = repository;
@@ -32,5 +39,18 @@ public class StudentsService implements IStudentService {
     public Student getStudentById(Long id) {
         if (id > 0) return repository.getById(id);
         else return null;
+    }
+
+    public void actualizarEscuelaYEstudiante(Long id, String nombre, int curso){
+
+        School escuela = repoEscuela.getById(id);
+        escuela.setName(nombre);
+        List<Student> stds = escuela.getEstudiantes();
+
+        for (Student std : stds) {
+            std.setCurso(curso);
+            repository.update(std);
+        }
+
     }
 }
