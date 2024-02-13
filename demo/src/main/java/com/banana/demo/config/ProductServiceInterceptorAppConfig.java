ed46c6695765f4944bee565703754b7d0137f930
Ricardo <ricardo@enmotionvalue.com>
2024-02-13 10:00:03 +0100

    update

---
+++ demo/src/main/java/com/banana/demo/config/ProductServiceInterceptorAppConfig.java
@@ -0,0 +1,18 @@
+package com.banana.demo.config;
+
+import com.banana.demo.interceptor.ProductServiceInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class ProductServiceInterceptorAppConfig implements WebMvcConfigurer {
+    @Autowired
+    ProductServiceInterceptor productServiceInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(productServiceInterceptor);
+    }
+}
---
+++ demo/src/main/java/com/banana/demo/interceptor/ProductServiceInterceptor.java
@@ -0,0 +1,86 @@
+package com.banana.demo.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+@Component
+public class ProductServiceInterceptor implements HandlerInterceptor {
+    private static Logger log = LoggerFactory.getLogger(ProductServiceInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        System.out.println("Pre Handle method is Calling");
+        log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
+                + "]" + request.getRequestURI() + getParameters(request));
+
+        response.addHeader("My-header","Ricardo Header");
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
+
+        System.out.println("Post Handle method is Calling");
+        log.info("[postHandle][" + response + "]");
+
+
+    }
+
+    @Override
+    public void afterCompletion
+            (HttpServletRequest request, HttpServletResponse response, Object
+                    handler, Exception exception) throws Exception {
+
+        System.out.println("Request and Response is completed");
+        log.info("[afterCompletion][" + request + "][exception: " + exception + "]");
+
+    }
+
+    private String getParameters(HttpServletRequest request) {
+        StringBuffer posted = new StringBuffer();
+        Enumeration<?> e = request.getParameterNames();
+        if (e != null) {
+            posted.append("?");
+        }
+        while (e.hasMoreElements()) {
+            if (posted.length() > 1) {
+                posted.append("&");
+            }
+            String curr = (String) e.nextElement();
+            posted.append(curr + "=");
+            if (curr.contains("password")
+                    || curr.contains("pass")
+                    || curr.contains("pwd")) {
+                posted.append("*****");
+            } else {
+                posted.append(request.getParameter(curr));
+            }
+        }
+        String ip = request.getHeader("X-FORWARDED-FOR");
+        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+        if (ipAddr != null && !ipAddr.equals("")) {
+            posted.append("&_psip=" + ipAddr);
+        }
+        return posted.toString();
+    }
+
+    private String getRemoteAddr(HttpServletRequest request) {
+        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+        if (ipFromHeader != null && ipFromHeader.length() > 0) {
+            log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+            return ipFromHeader;
+        }
+        return request.getRemoteAddr();
+    }
+}
