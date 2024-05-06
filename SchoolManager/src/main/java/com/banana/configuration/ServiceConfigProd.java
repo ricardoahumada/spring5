6477591acc6163acad12f17744b48ac909e71b17
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-06 13:10:56 +0200

    update

--- SchoolManager/src/main/java/com/banana/configuration/ServiceConfigProd.java
+++ SchoolManager/src/main/java/com/banana/configuration/ServiceConfigProd.java
@@ -1,5 +1,7 @@
 package com.banana.configuration;
 
+import com.banana.services.IStudentService;
+import com.banana.services.StudentsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
