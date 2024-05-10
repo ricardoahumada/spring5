2e1b7dbc806c1c89a9e9247d7229b2ac942f57a3
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-10 09:47:14 +0200

    update

--- SchoolManagerJPA/src/test/java/com/banana/AppTest.java
+++ SchoolManagerJPA/src/test/java/com/banana/AppTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @EnableAutoConfiguration
 public class AppTest {
 
-//    @PersistenceContext
+    @PersistenceContext
     EntityManager em;
 
     @Test
