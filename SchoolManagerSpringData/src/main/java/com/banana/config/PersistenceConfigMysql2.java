1f8850244e4206c508ed832bde73ae440ab2d6c8
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-05-15 12:00:52 +0200

    update

---
+++ SchoolManagerSpringData/src/main/java/com/banana/config/PersistenceConfigMysql2.java
@@ -0,0 +1,79 @@
+package com.banana.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "entityManagerFactoryMysql2",
+        basePackages = {"com.banana.persistence.project"},
+        transactionManagerRef = "transactionManagerMysql2"
+)
+public class PersistenceConfigMysql2 {
+    @Autowired
+    private Environment env;
+
+    @Bean(name = "transactionManagerMysql2")
+    public PlatformTransactionManager transactionManagerMysql2() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactoryMysql2().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    DataSource schoolDataSourceMysql2() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setUrl(env.getProperty("mysql2.datasource.url"));
+
+        ds.setDriverClassName(env.getProperty("mysql2.datasource.driver.class"));
+        ds.setUsername(env.getProperty("mysql2.datasource.username"));
+        ds.setPassword(env.getProperty("mysql2.datasource.password"));
+
+        return ds;
+    }
+
+    @Bean
+    public JpaVendorAdapter vendorAdapter() {
+        HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
+        va.setShowSql(true);
+        va.setGenerateDdl(true);
+
+        return va;
+    }
+
+    @Bean(name = "entityManagerFactoryMysql2")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryMysql2() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+        em.setPersistenceUnitName("school-mysql2");
+
+        em.setDataSource(schoolDataSourceMysql2());
+        em.setPackagesToScan("com.banana.models");
+        em.setJpaVendorAdapter(vendorAdapter());
+        em.setJpaProperties(additionalProperties());
+        return em;
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", env.getProperty("mysql2.jpa.properties.hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("mysql2.jpa.hibernate.ddl-auto"));
+        return properties;
+    }
+
+}
