01751f0b167c085c3bfbda7368c965d202dbebb0
ricardoag@expert.ntmind.es <ricardo>
2024-09-03 16:50:37 +0200

    update

--- SchoolManagerHbnt/src/main/java/com/banana/models/Student.java
+++ SchoolManagerHbnt/src/main/java/com/banana/models/Student.java
@@ -2,19 +2,26 @@ package com.banana.models;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-
+@Entity(name = "estudiante")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="name")
     private String nombre;
 
+    @Column(name="surname")
     private String apellido;
 
+    @Column(name="course")
     private int curso;
 
     public boolean isValid() {
