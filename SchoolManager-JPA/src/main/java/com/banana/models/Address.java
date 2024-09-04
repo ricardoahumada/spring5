79c5191f5758eafa6be9675bcb0038db1ca7f185
ricardoag@expert.ntmind.es <ricardo>
2024-09-04 14:58:30 +0200

    update

---
+++ SchoolManager-JPA/src/main/java/com/banana/models/Address.java
@@ -0,0 +1,22 @@
+package com.banana.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "direccion")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullAddress;
+
+}
