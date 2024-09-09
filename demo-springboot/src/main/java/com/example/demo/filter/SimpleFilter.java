22243d49edb7392967de2dd41b6a5ac2af9463cb
ricardoag@expert.ntmind.es <ricardo>
2024-09-09 16:30:32 +0200

    update

---
+++ demo-springboot/src/main/java/com/example/demo/filter/SimpleFilter.java
@@ -0,0 +1,32 @@
+package com.example.demo.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class SimpleFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("Inicializando...");
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        logger.info("Filtrando 1..."+ servletRequest.getLocalAddr()+"::"+servletRequest.getRemoteHost());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Destruyendo...");
+    }
+}
