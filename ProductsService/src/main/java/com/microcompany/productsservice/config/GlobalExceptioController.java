e7f15af8f65a76b10b89f0ecbb77793ab225d8a8
ricardoag@expert.ntmind.es <ricardo>
2024-09-12 16:32:50 +0200

    update

--- ProductsService/src/main/java/com/microcompany/productsservice/config/GlobalExceptioController.java
+++ ProductsService/src/main/java/com/microcompany/productsservice/config/GlobalExceptioController.java
@@ -5,6 +5,7 @@ import com.microcompany.productsservice.model.StatusMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
--- ProductsService/src/main/java/com/microcompany/productsservice/exception/NewProductException.java
+++ ProductsService/src/main/java/com/microcompany/productsservice/exception/NewProductException.java
@@ -1,10 +1,10 @@
 package com.microcompany.productsservice.exception;
 
 
-public class ProductNotfoundException extends RuntimeException {
+public class NewProductException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public ProductNotfoundException(String message) {
+    public NewProductException(String message) {
         super(message);
     }
 }
