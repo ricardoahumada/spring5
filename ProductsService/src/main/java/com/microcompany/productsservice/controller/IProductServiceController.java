8aa74ade0b043758c82dadfe0d96e386ab184ac8
ricardoag@expert.ntmind.es <ricardo>
2024-09-16 11:22:39 +0200

    update

--- ProductsService/src/main/java/com/microcompany/productsservice/controller/IProductServiceController.java
+++ ProductsService/src/main/java/com/microcompany/productsservice/controller/IProductServiceController.java
@@ -1,6 +1,10 @@
 package com.microcompany.productsservice.controller;
 
 import com.microcompany.productsservice.model.Product;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,11 +23,28 @@ public interface IProductServiceController {
     public List<Product> getAll(@RequestParam(required = false) String text);
 
 
+    @Operation(summary = "Obtener un producto", description = "Endpoint para obtener un producto concreto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo bien"),
+            @ApiResponse(responseCode = "404", description = "El producto no existe")
+    })
     @RequestMapping(value = "/{pid}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Product> getOne(@Min(1) @PathVariable("pid") Long id);
+    public ResponseEntity<Product> getOne(
+            @Parameter(name = "Id", description = "El identificador del producto", required = true, example = "234")
+            @Min(1) @PathVariable("pid") Long id
+    );
 
+    @Operation(summary = "Crear un producto", description = "Endpoint para crear un producto concreto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Todo bien"),
+            @ApiResponse(responseCode = "412", description = "El producto enviado no es correcto")
+    })
     @RequestMapping(value = "", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Product> create(@Valid @RequestBody Product product);
+    public ResponseEntity<Product> create(
+            @Valid @RequestBody
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, description = "El producto a crear sin Id.")
+            Product product
+    );
 
     @RequestMapping(value = "/{pid}", method = RequestMethod.PUT)
     public ResponseEntity updateProduct(@PathVariable("pid") Long id, @RequestBody Product aProduct);
--- ProductsService/src/main/java/com/microcompany/productsservice/model/Product.java
+++ ProductsService/src/main/java/com/microcompany/productsservice/model/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.*;
 
 import com.microcompany.productsservice.constraints.SerialNumber;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Entity
@@ -11,18 +12,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Schema(name = "Modelo de producto", description = "Representa un producto a intercambiar")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(name = "Id", description = "El identificador único del producto. Debe ser un número mayor que 0. No es requerido en POST y PUT", required = false, example = "234")
     private Long id;
 
-    @NotNull
+//    @NotNull
     @NotEmpty
     @NotBlank(message = "Debe tener un valor")
     @Size(min = 3, max = 20, message = "Debe ser entre 3 y 20 cars")
     private String name;
 
-    @NonNull
+//    @NonNull
     @NotBlank
 //    @Pattern(regexp = "[1-9]{3}-[1-9]{3}-[1-9]{4}", message = "Debe tener la forma de 111-222-3333")
     @SerialNumber
@@ -31,4 +34,6 @@ public class Product {
     public void isValid() throws Exception {
         if (name == null || serial == null) throw new Exception("Producto no válido");
     }
+
+
 }
