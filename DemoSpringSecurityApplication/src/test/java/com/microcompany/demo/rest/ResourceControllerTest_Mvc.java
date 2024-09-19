983a68f4f5ff1e15ef869bce6d9180c37c61de5d
ricardoag@expert.ntmind.es <ricardo>
2024-09-19 10:37:49 +0200

    update

--- DemoSpringSecurityApplication/src/test/java/com/microcompany/demo/rest/ResourceControllerTest_Mvc.java
+++ DemoSpringSecurityApplication/src/test/java/com/microcompany/demo/rest/ResourceControllerTest_Mvc.java
@@ -1,12 +1,10 @@
 package com.microcompany.demo.rest;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -15,13 +13,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(ResourceController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class ResourceControllerTest_Mvc {
 
     @Autowired
     private MockMvc mockMvc;
-
 
     @Test
     public void getResource() throws Exception {
--- DemoSpringSecurityApplication/src/test/java/com/microcompany/demo/rest/ResourceControllerTest_WebMvcTest.java
+++ DemoSpringSecurityApplication/src/test/java/com/microcompany/demo/rest/ResourceControllerTest_WebMvcTest.java
@@ -3,11 +3,8 @@ package com.microcompany.demo.rest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,12 +14,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ResourceController.class)
 class ResourceControllerTest_WebMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
+
 
     @Test
     public void getResource() throws Exception {
